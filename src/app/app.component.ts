2d2cb5d2d35a22b58f7709c5fa0338a38c71290b
Udhayarajan J <udhayarajan.j@shauryatechnosoft.com>
2022-11-06 19:05:31 +0530

    To Add Some Template HTML Code And App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pdf-angular-14';
+export class AppComponent implements OnInit {
+ //Declare Property
+ pdfFormGroup!: FormGroup;
+ isSubmitted: boolean = false;
+
+ @ViewChild('pdfFormModalOpen') pdfFormModalOpen!: ElementRef;
+
+
+ constructor(private fb: FormBuilder)
+ {
+
+ }
+ ngOnInit(): void
+ {
+   this.loadFormIntialization();
+ }
+
+
+ //Submit The Form Value 
+ onSubmit()
+ {
+   this.isSubmitted = true;
+   if (!this.pdfFormGroup.valid)
+     return;
+   this.pdfFormModalOpen.nativeElement.click();
+ }
+
+
+ //To Declare The Forms
+ loadFormIntialization()
+ {
+   this.pdfFormGroup = this.fb.group({
+     studentName: ['', Validators.required],
+     rollNumber: ['', Validators.required],
+     departmentName: ['', Validators.required],
+     collageName: ['', Validators.required],
+     collageCity: ['', Validators.required]
+   });
+ }
+
+ //Get Whether Form Error Is There Or Not
+ get f()
+ {
+   return this.pdfFormGroup.controls;
+ }
+
+ //clearForm
+ clearForm()
+ {
+   this.loadFormIntialization();
+   this.isSubmitted = false;
+ }
 }
